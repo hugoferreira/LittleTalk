6b3d6037bbdfd980646bea792c25bc050cd067b2
Hugo Ferreira <bytter@gmail.com>
2010-11-21 02:36:03 +0000

    * Improved TestInterpreterChain.

--- LittleTalk/VirtualMachineTests.cs
+++ LittleTalk/VirtualMachineTests.cs
@@ -14,29 +14,36 @@ namespace LittleTalk {
 	
 	[TestFixture()]
 	public class VirtualMachineTests {
+		const int anInteger = 32;
+		
 		[Test()]
 		public void TestLoadStore () {
 			var p = new MockProcess();
 			var i = new Interpreter(p, null);
-			i.ops.Add(new LoadConstant(42));
+			i.ops.Add(new LoadConstant(anInteger));
 			i.ops.Add(new StoreLocalVar("temp"));
 			i.ops.Add(new LoadLocalVar("temp"));
 			i.Resume();
 			
 			Assert.AreEqual(typeof(NativeInteger), i.stack.Peek().GetType());
-			Assert.AreEqual(42, ((NativeInteger) i.stack.Peek()).Value);
+			Assert.AreEqual(anInteger, ((NativeInteger) i.stack.Peek()).Value);
 		}
 		
 		[Test()]
-		public void Test() {
+		public void TestInterpreterChain() {
 			var p = new MockProcess();
 			var i1 = new Interpreter(p, null);
 			var i2 = new Interpreter(p, i1);
-			var val = new NativeInteger(32);
-			// i2.ops.Add();
+			
+			i2.ops.Add(new LoadConstant(anInteger));
 			i2.ops.Add(new Return());
 			i2.Resume();
-			Assert.AreEqual(val, i1.stack.Peek()); 
+			
+			Assert.AreEqual(i1, p.i);
+			
+			var peek = i1.stack.Peek(); 
+			Assert.AreEqual(typeof(NativeInteger), peek.GetType()); 
+			Assert.AreEqual(anInteger, ((NativeInteger) peek).Value);
 		}
 	}
 }
